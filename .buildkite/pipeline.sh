d3665252b61cfde4e6d62f8c8c157888e00168a4
Steve Kirkland <steve@bugsnag.com>
2021-06-14 11:15:39 +0100

    Buildkite pipelines

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if [[ "$SERVER_TEST" == "11" ]]; then
+  buildkite-agent pipeline upload .buildkite/macos-11.yml
+  buildkite-agent pipeline upload .buildkite/build-test-fixtures.yml
+# Placeholders for 10.15 and earlier
+#elif [[ "$SERVER_TEST" == "10.15" ]]; then
+#  buildkite-agent pipeline upload .buildkite/macos-10.15.yml
+#elif [[ "$SERVER_TEST" == "10.14" ]]; then
+#  buildkite-agent pipeline upload .buildkite/macos-10.14.yml
+#elif [[ "$SERVER_TEST" == "10.13" ]]; then
+#  buildkite-agent pipeline upload .buildkite/macos-10.13.yml
+else
+  echo "SERVER_TEST not set to recognised value"
+fi
