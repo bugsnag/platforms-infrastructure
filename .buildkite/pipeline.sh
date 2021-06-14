284832a0540b6e609788fbf27e768058f228d3c6
Steve Kirkland <steve@bugsnag.com>
2021-06-14 15:35:36 +0100

    Add tests for 10.15

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,9 +3,9 @@
 if [[ "$SERVER_TEST" == "11" ]]; then
   buildkite-agent pipeline upload .buildkite/macos-11.yml
   buildkite-agent pipeline upload .buildkite/build-test-fixtures.yml
-# Placeholders for 10.15 and earlier
-#elif [[ "$SERVER_TEST" == "10.15" ]]; then
-#  buildkite-agent pipeline upload .buildkite/macos-10.15.yml
+elif [[ "$SERVER_TEST" == "10.15" ]]; then
+  buildkite-agent pipeline upload .buildkite/macos-10.15.yml
+  buildkite-agent pipeline upload .buildkite/build-test-fixtures.yml
 #elif [[ "$SERVER_TEST" == "10.14" ]]; then
 #  buildkite-agent pipeline upload .buildkite/macos-10.14.yml
 #elif [[ "$SERVER_TEST" == "10.13" ]]; then
