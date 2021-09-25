f1454b8d4ad44c6c01b32198f6d20723a97b0578
Steve Kirkland-Walton <steve@bugsnag.com>
2021-09-25 21:15:36 +0100

    Add tools for Linting

--- mac-servers/general/setup-server.sh
+++ mac-servers/general/setup-server.sh
@@ -49,6 +49,7 @@ brew install npm
 brew install carthage
 brew install rbenv
 brew install ruby-build
+brew install clang-format
 
 echo Node 14
 brew unlink node
@@ -107,6 +108,7 @@ echo Installing global NPM packages
 echo ==============================
 npm install -g turtle-cli@0.17.3 # Used to build Expo
 npm install -g appium # Used to run macOS end-to-end tests
+npm install -g cspell
 
 echo =========================
 echo Installing appium-for-mac
